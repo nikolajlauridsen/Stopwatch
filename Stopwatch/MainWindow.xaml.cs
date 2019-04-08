f459c238324cd29357edabc5b3545ab7dc28f43f
Molex <simplesleeptimer@gmail.com>
2019-04-08 11:20:30 +0200

    Add project files.

---
+++ Stopwatch/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stopwatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StopWatch _watch;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _watch = new StopWatch(DispatchUpdate);
+            StartBtn.Click += (sender, e) => _watch.Start();
+            PauseBtn.Click += (sender, e) => _watch.Pause();
+            StopBtn.Click += (sender, e) => _watch.Stop();
+        }
+
+        private void DispatchUpdate(TimeSpan time)
+        {
+            try
+            {
+                timeLbl.Dispatcher.Invoke(() => UpdateLabel(time));
+            }
+            catch (TaskCanceledException)
+            {
+
+            }
+            
+        }
+
+        private void UpdateLabel(TimeSpan time)
+        {
+            timeLbl.Content = time.ToString(@"hh\:mm\:ss\.ff");
+        }
+    }
+}
---
+++ Stopwatch/StopWatch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class StopWatch
+    {
+        public TimeSpan Elapsed
+        {
+            get
+            {
+                TimeSpan baseSpan = DateTime.Now.Subtract(_startTime);
+                foreach (TimeSpan span in _pauses)
+                {
+                    baseSpan += span;
+                }
+
+                return baseSpan;
+            }
+        }
+
+        public int UpdateDelay = 50;
+
+        private bool _running = false;
+        private DateTime _startTime;
+        private List<TimeSpan> _pauses = new List<TimeSpan>();
+        private Action<TimeSpan> _updateAction = null;
+
+
+        public StopWatch(Action<TimeSpan> UpdateAction)
+        {
+            _updateAction = UpdateAction;
+        }
+
+        public StopWatch() : this(null)
+        {
+
+        }
+
+        public void Start()
+        {
+            if (!_running)
+            {
+                _startTime = DateTime.Now;
+                _running = true;
+                if (_updateAction != null)
+                {
+                    new Thread(_updateWork).Start();
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            _running = false;
+            _pauses.Clear();
+        }
+
+        public void Pause()
+        {
+            _running = false;
+            _pauses.Add(DateTime.Now.Subtract(_startTime));
+        }
+
+        private void _updateWork()
+        {
+            while (_running)
+            {
+                _updateAction(Elapsed);
+                Thread.Sleep(UpdateDelay);
+            }
+        }
+
+
+    }
+}
