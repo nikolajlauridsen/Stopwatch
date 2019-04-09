e2c0e8465608c307639d70e820397357e859ac43
Molex <simplesleeptimer@gmail.com>
2019-04-09 14:30:17 +0200

    Add HookManager project

---
+++ KeyboardHook/Hook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+
+namespace KeyboardHook
+{
+    internal class Hook
+    {
+        internal Key TargetKey;
+        private Action<Key> _keyAction;
+        private bool _singleFire;
+        private bool _pressed = false;
+
+        internal Hook(Key targeKey, Action<Key> keyAction, bool singleFire = true)
+        {
+            TargetKey = targeKey;
+            _keyAction = keyAction;
+            _singleFire = singleFire;
+        }
+
+        internal void Checkstatus()
+        {
+            if (Keyboard.IsKeyDown(TargetKey) && (!_pressed || !_singleFire))
+            {
+                _keyAction(TargetKey);
+                _pressed = true;
+            } else if (_pressed)
+            {
+                _pressed = false;
+            }
+        }
+
+    }
+}
---
+++ KeyboardHook/HookManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KeyboardHook
+{
+    public class HookManager
+    {
+        private List<Hook> _hooks = new List<Hook>();
+        private bool _listening;
+        public int GracePeriod = 50;
+
+        public void RegisterHook(Key targetKey, Action<Key> keyAction, bool singleFire = true)
+        {
+            _hooks.Add(new Hook(targetKey, keyAction, singleFire));
+        }
+
+        public void DisableHook(Key targetKey)
+        {
+            _hooks.Remove(_hooks.Find(hook => hook.TargetKey == targetKey));
+        }
+
+        public void Listen()
+        {
+            if (!_listening)
+            {
+                _listening = true;
+                new Thread(checkHooks).Start();
+            }
+            else
+            {
+                throw new Exception("HookManager can not listen more than once.");
+            }
+
+        }
+
+        public void StopListening()
+        {
+            _listening = false;
+        }
+
+        public void ClearHooks()
+        {
+            _hooks.Clear();
+        }
+
+        private void checkHooks()
+        {
+            while (_listening)
+            {
+                Thread.Sleep(GracePeriod);
+                foreach (Hook hook in _hooks) {
+                    hook.Checkstatus();
+                }
+            }
+        }
+    }
+}
