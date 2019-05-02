882608044775608dc7f56f87dac32d06bfa4b5fe
Molex <simplesleeptimer@gmail.com>
2019-05-02 08:42:01 +0200

    Allow users to change between timers Also tweak UI slightly

--- Stopwatch/MainWindow.xaml.cs
+++ Stopwatch/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 using KeyboardHook;
@@ -19,16 +21,36 @@ namespace Stopwatch
         private CountdownTimer _timer;
         private HookManager _hooks;
 
+        private int _selectedPage;
+        private List<Page> _pages = new List<Page>();
+
         public MainWindow()
         {
             InitializeComponent();
             _watch = new StopWatch();
             _timer = new CountdownTimer();
             _hooks = new HookManager();
-            // ContentFame.Navigate(new StopwatchPage(_watch, _hooks));
-            ContentFame.Navigate(new CountdownPage(_timer));
+            _pages.Add(new StopwatchPage(_watch, _hooks));
+            _pages.Add(new CountdownPage(_timer));
+            ContentFrame.Navigate(_pages[0]);
+            
 
             SettingsBtn.Click += (sender, e) => new SettingsWindow(_watch, _hooks).Show();
+
+            SwapBtn.Click += (sender, e) =>
+            {
+                if (_selectedPage == 0)
+                {
+                    ContentFrame.Navigate(_pages[1]);
+                    _selectedPage = 1;
+                    SwapBtn.Content = "Stopwatch";
+                } else if (_selectedPage == 1)
+                {
+                    ContentFrame.Navigate(_pages[0]);
+                    _selectedPage = 0;
+                    SwapBtn.Content = "Timer";
+                }
+            };
         }
         
     }
